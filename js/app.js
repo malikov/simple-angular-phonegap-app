a939cf39bb912d224a06465d103abc78a2a4625f
malikov <malikovincent@gmail.com>
2014-02-05 22:03:14 -0500

    added an interceptor to monitor requests and responses

--- js/app.js
+++ js/app.js
@@ -8,8 +8,8 @@ angular.module('MobileDev', [
 	'malikov.phonegap.splashscreen',
 	'malikov.phonegap.storage'
 ])
-.config(['$routeProvider','$locationProvider','$provide',
-	function($routeProvider,$locationProvider,$provide){
+.config(['$routeProvider','$locationProvider','$provide','$httpProvider',
+	function($routeProvider,$locationProvider,$provide,$httpProvider){
 	/*
 		Define interceptors for the system here
 	*/
@@ -21,6 +21,78 @@ angular.module('MobileDev', [
 
 	console.log('log locationProvider');
 	console.log($locationProvider);
+
+	/*
+		creating a provider to intercept errors or 401 requests
+	*/
+
+	/*
+		interceptor
+	*/
+	$provide.factory('mobileDevInterceptor',function($q, $rootScope){
+		console.log($q);
+		console.log($rootScope);
+		
+		var interceptor = {
+			/*
+				Catches request before it is sent
+			*/
+			request : function(config){
+				console.log("Request");
+				console.log(config);
+					
+				/*
+					handle requests here
+				*/
+					
+				return config || $q.when(config);
+			},
+			
+			/*
+				Catches errors when request fails
+			*/
+			requestError : function(requestError){
+				console.log("RequestError");
+				console.log(requestError);
+				
+				/*
+					handle error here
+				*/
+				
+				return $q.reject(requestError);
+			},
+			
+			/*
+				Handles response success
+			*/
+			response : function(response){
+				console.log("Response");
+				console.log(response);
+				
+				return response || $q.when(response);
+			},
+			
+			/*
+				Handles response failure
+			*/
+			responseError : function(error){
+				console.log("ResponseError");
+				console.log("Error status : "+error.status);
+				console.log(error);
+				
+				return $q.reject(error);
+			}			
+		};
+		
+		return interceptor;
+		
+	  });
+	  
+	  
+	  /*
+		Adding interceptor to the httpProvider
+	  */
+	  $httpProvider.interceptors.push('mobileDevInterceptor');
 
 }])
 .run(['$rootScope','splashscreen',
